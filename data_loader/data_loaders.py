817b4a88c9dd62bf5aae8e313912ee9b2680596a
Vince <vincivenv@gmail.com>
2021-12-12 15:59:28 +0300

    data transforms

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -4,26 +4,28 @@ from base import BaseDataLoader
 
 
 class AjimeDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
+    """AjimeDataLoader"""
 
     def __init__(
         self,
         data_dir,
         batch_size,
         shuffle=True,
-        validation_split=0.0,
-        num_workers=1,
+        validation_split=0.2,
+        num_workers=4,
         training=True,
     ):
         trsfm = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
         )
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(
-            self.data_dir, train=training, download=True, transform=trsfm
-        )
+        self.dataset = datasets.ImageFolder(data_dir, transform=trsfm)
         super().__init__(
             self.dataset, batch_size, shuffle, validation_split, num_workers
         )
